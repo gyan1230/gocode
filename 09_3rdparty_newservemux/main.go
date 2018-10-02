c053d3d6b9c3f8f8e550dab7bc7ed1abf2255100
Gyan chand Mohanty <gyanchand.mohanty@gmail.com>
2018-10-02 08:41:37 +0530

    3rd party mux package

--- 09_3rdparty_newservemux/main.go
+++ 09_3rdparty_newservemux/main.go
@@ -7,18 +7,18 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-func index(w http.ResponseWriter, r *http.Request) {
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	io.WriteString(w, "I am in Root...")
 }
 
-func gyan(w http.ResponseWriter, r *http.Request) {
+func Gyan(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	io.WriteString(w, "Hi gyan...")
 }
 
 func main() {
-	newRouter := httprouter.New
-	newRouter.Get("/", index)
-	newRouter.Get("/gyan", gyan)
+	newRouter := httprouter.New()
+	newRouter.GET("/", Index)
+	newRouter.GET("/gyan", Gyan)
 
 	http.ListenAndServe(":8080", newRouter)
 }
